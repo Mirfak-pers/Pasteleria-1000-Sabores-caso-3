8f418c16cabcc37fb17ad0b3c2a76dabc0b6f5d9
Biiancoo <bia.martinez@duocuc.cl>
2025-09-30 18:17:47 -0300

    Integracion firebase a react
    se enlaza el SDK del proyecto firebase y se crea el service para apuntar a la coleccion "usuario"

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC01DeLX515dsD29to5rHeqaWC8RV98KNg",
+  authDomain: "tiendapasteleriamilsabor-a7ac6.firebaseapp.com",
+  projectId: "tiendapasteleriamilsabor-a7ac6",
+  storageBucket: "tiendapasteleriamilsabor-a7ac6.firebasestorage.app",
+  messagingSenderId: "522171765461",
+  appId: "1:522171765461:web:6745850bf2a9735682885c",
+  measurementId: "G-08JFT3CMHR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
